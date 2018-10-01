49536b03754ea7dbf0a77ddec69223620992c7a2
rrpatil49 <rrpatil49@gmail.com>
2018-10-01 16:21:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,27 +11,23 @@ if($method == 'POST'){
 
 	switch ($text) {
 		case 'hi':
-			$speech = "Hi, Nice to meet you";
+			$json->queryResult->fulfillmentMessages->text->text = "Hi, Nice to meet you";
 			break;
 
 		case 'bye':
-			$speech = "Bye, good night";
+			$json->queryResult->fulfillmentMessages->text->text = "Bye, good night";
 			break;
 
 		case 'anything':
-			$speech = "Yes, you can type anything here.";
+			$json->queryResult->fulfillmentMessages->text->text = "Yes, you can type anything here.";
 			break;
 		
 		default:
-			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			$json->queryResult->fulfillmentMessages->text->text = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
-
-	$response = new \stdClass();
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
-	echo json_encode($response);
+ 
+	echo json_encode($json);
 }
 else
 {
@@ -39,3 +35,4 @@ else
 }
 
 ?>
+
