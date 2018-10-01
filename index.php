904f6cfad395c2cecf889f4445cb3b51921cff37
rrpatil49 <rrpatil49@gmail.com>
2018-10-01 16:27:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->queryResult->intent->displayName;
+	$text = $json->queryResult->queryText;
 
 	switch ($text) {
 		case 'hi':
