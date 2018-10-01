3fc5f149e57f900a9b7a6dc7c6102d4fdca78730
rrpatil49 <rrpatil49@gmail.com>
2018-10-01 15:33:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
-if($method == 'GET'){
+if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	$text = $json->metadata->intentName->text; 
+	$text = $json->results->metadata->intentName;
 
 	switch ($text) {
 		case 'hi':
