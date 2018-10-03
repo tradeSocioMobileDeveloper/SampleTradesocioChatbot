ed5ad4fa4d93e67c5e3ccd065c53969d6a000161
rrpatil49 <rrpatil49@gmail.com>
2018-10-03 16:52:26 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,29 +2,64 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+
+$text_temp = $_REQUEST['text_temp'];
+function explodeKeyword($text)
+{
+	if (strpos($text, 'balance') !== false)
+	{
+		$getTxtArray = explode("==TSTEXT",$text);
+		$getBalanceArray = explode("::",$getTxtArray[1]);
+		 
+		if(in_array("profileID",$getBalanceArray)) $profileID = $getBalanceArray[2];
+		if(in_array("AccountID",$getBalanceArray)) $AccountID = $getBalanceArray[2];
+		
+		$keyword  = "balance";
+		if($profileID )  $output=" Balance of your Profile # ".$profileID ." is $ 1020 ";
+		if($AccountID) $output=" Balance of your account # ".$AccountID ." is $ 1020 ";
+		
+		return $output ; 
+		 
+	}
+}
+
+//STARTED ADDED FOR TESTING PURPOSE 
+if($text_temp !="")
+{
+	if (strpos($text_temp, 'balance') !== false)
+	{
+		echo explodeKeyword($text_temp) ; 
+		die;
+	}
+}
+
+	
 // Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-        
+
 	$text = $json->queryResult->queryText;
+	$keyword  = $text;
+	if(strstr($text,"balance"))
+	{
+		 $keyword  = "balance";
+	}
 	
-    if (strpos($text, 'balance') !== false)
-    {
-        $text = "balance";
-    }
-    
-	switch ($text) {
+	switch ($keyword) 
+	{
 		case 'balance':
-		$check->fulfillmentText = "Your balance is $1000 ";
+		$check->fulfillmentText = explodeKeyword($text) ;
 			//$check->displayText = "Hi, Nice to meet you";
 			//$check->source = "webhook-echo-sample";
 			break;
-        case 'hi':
-            $check->fulfillmentText = "Hi, Its a worderful day , welcome to the Tradesocio ";
-            //$check->displayText = "Hi, Nice to meet you";
-            //$check->source = "webhook-echo-sample";
-            break;
+	
+		case 'hi':
+		$check->fulfillmentText = "Hi, Its a worderful day , welcome to the Tradesocio ";
+			//$check->displayText = "Hi, Nice to meet you";
+			//$check->source = "webhook-echo-sample";
+			break;
+		
 		case 'bye-bye':
 		$check->fulfillmentText = "Hi...... bye bye";
 		//$check->displayText = "Hi, Nice to meet you";
